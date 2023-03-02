024da7e863c7d2c5b85239bf465f2a43b34c0d05
Emily Reyes <emilymwind1@gmail.com>
2023-03-01 21:41:34 -0700

    Add project files.

---
+++ Mission09_emwind/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_emwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_emwind.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var bookList = repo.Books.ToList();
+
+            return View(bookList);
+        }
+
+    }
+}
