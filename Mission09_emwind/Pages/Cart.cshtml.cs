a36a4e869a61a643c639482a5b707fc4c7ed8dbb
Emily Reyes <emilymwind1@gmail.com>
2023-03-13 21:40:09 -0600

    Remaining mission 10 content

--- Mission09_emwind/Pages/Cart.cshtml.cs
+++ Mission09_emwind/Pages/Cart.cshtml.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission09_emwind.Models;
 
 namespace Mission09_emwind.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private IBookstoreRepository repo { get; set; }
+
+        public CartModel (IBookstoreRepository temp)
         {
+            repo = temp;
+        }
+        public Basket basket { get; set; }
+
+        public void OnGet(Basket b)
+        {
+            basket = b;
+        }
+
+        public IActionResult onPost(int bookId)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            basket = new Basket();
+
+            basket.AddItem(b, 1);
+
+            return RedirectToPage(basket);
         }
     }
 }
