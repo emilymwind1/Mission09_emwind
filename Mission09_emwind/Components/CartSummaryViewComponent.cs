06fd7b88b7bf3b195ee4d70ed3c05442a40bafbf
Emily Reyes <emilymwind1@gmail.com>
2023-03-16 18:22:07 -0600

    Adding cart icon

---
+++ Mission09_emwind/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_emwind.Models;
+
+namespace Mission09_emwind.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
